c37826f0304e7b774ad70736cb953943811d51b0
Baisampayan <baisampayandas@gmail.com>
2024-06-09 14:48:52 +0530

    Adding PHP Operators

--- PHP/CH 9 - Operators/index.php
+++ PHP/CH 9 - Operators/index.php
@@ -1,4 +1,49 @@
 <?php
 
-// Arithmetic Operators
-echo (8+20);
+// Arithmetic Operators (+. -, /. *. %, and **)
+    $num1 = 10;
+    $num2 = 25;
+
+    # Addition Operators
+    echo $num1 + $num2 . '<br>';
+
+    # Subtraction Operators
+    echo $num2 - $num1 . '<br>';
+
+    # Multiplication Operators
+    echo $num1 * $num2 . '<br>';
+
+    # Division Operators
+    echo $num2 / $num1 . '<br>';
+
+    # Modulus Operators
+    echo $num2 % $num1 . '<br>';
+
+    # Exponentiation Operators
+    echo $num1 ** $num1 . '<br><br>';
+
+// Assignment Operators
+    $num1 = 15;
+    echo $num1 . '<br>';
+
+    # Addition Assignment Operators
+    echo ($num1 + $num2) . '<br>';
+    echo ($num1 += $num2) . '<br>';
+
+    # Subtraction Assignment Operators
+    echo ($num1 - $num2) . '<br>';
+    echo ($num1 -= $num2) . '<br>';
+
+    # Multiplication Assignment Operators
+    echo ($num1 * $num2) . '<br>';
+    echo ($num1 *= $num2) . '<br>';
+
+    # Division Assignment Operators
+    echo ($num1 / $num2) . '<br>';
+    echo ($num1 /= $num2) . '<br>';
+
+    # Modulus Assignment Operators
+    echo ($num2 % $num1) . '<br>';
+    echo ($num2 %= $num1) . '<br>';
+
+// Comparison Operators
