736469c07054d8fa97db2bf05c2087e90b8e1dfa
Baisampayan <baisampayandas@gmail.com>
2024-06-09 15:15:35 +0530

    Adding PHP Operators

--- PHP/CH 9 - Operators/index.php
+++ PHP/CH 9 - Operators/index.php
@@ -44,6 +44,65 @@
 
     # Modulus Assignment Operators
     echo ($num2 % $num1) . '<br>';
-    echo ($num2 %= $num1) . '<br>';
+    echo ($num2 %= $num1) . '<br><br>';
 
 // Comparison Operators
+    $num1 = 10;
+    $num2 = 10;
+
+    # Equal Operators
+    if($num1 == $num2) {
+        echo 'Equal' . '<br>';
+    } else {
+        echo 'Both the numbers are not equal.' . '<br>';
+    }
+
+    # Identical Operators
+    $str1 = 'Hi';
+    $str2 = 'Hi';
+    if($str1 === $str2) {
+        echo 'Identical' . '<br>';
+    } else {
+        echo 'Both the values are not Identical.' . '<br>';
+    }
+
+    # Not equal Operators
+    $num2 = 15;
+    if($num1 != $num2) {
+        echo 'Both the numbers are not equal.' . '<br>';
+    } else {
+        echo 'Both the numbers are equal.' . '<br>';
+    }
+
+    # Not equal Operators
+    if($num1 <> $num2) {
+        echo 'Both the numbers are not equal.' . '<br>';
+    } else {
+        echo 'Both the numbers are equal.' . '<br>';
+    }
+
+    # Not Identical Operators
+    $str1 = 'Hi';
+    $str2 = 'hi';
+    if($str1 !== $str2) {
+        echo 'Both the values are not Identical.' . '<br>';
+    } else {
+        echo 'Both the values are Identical.' . '<br>';
+    }
+
+    # Greater than Operators
+    if($num1 > $num2) {
+        echo 'First Number is greater than Second Number.' . '<br>';
+    } else {
+        echo 'Second Number is greater than First Number.' . '<br>';
+    }
+
+    # Less than Operators
+    if($num1 < $num2) {
+        echo 'First Number is Less than Second Number.' . '<br>';
+    } else {
+        echo 'Second Number is greater than First Number.' . '<br>';
+    }
+
+    # Greater than or equal to Operator
+    
