d0bd41f2bc91cc3e727eb23d05c12e54f229d5f9
Baisampayan <baisampayandas@gmail.com>
2024-06-19 01:28:37 +0530

    Added: Loops

--- PHP/CH 11 - Loops/index.php
+++ PHP/CH 11 - Loops/index.php
@@ -8,7 +8,14 @@ while($i <= 15) {
 }
 
 // Do-While Loops
+$i = 5;
+do {
+    echo $i . '<br>';
+} while ($i < 4);
 
 // For Loops
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
 
 // Foreach Loops
