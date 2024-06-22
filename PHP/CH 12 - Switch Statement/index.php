7129658fcf217c15cfc51e1ec66ee0abb23599ae
Baisampayan <baisampayandas@gmail.com>
2024-06-22 13:28:01 +0530

    Added: Switch

--- PHP/CH 12 - Switch Statement/index.php
+++ PHP/CH 12 - Switch Statement/index.php
@@ -1,19 +1,45 @@
 <?php
 
 // Switch Statement
- $payment_status = 'PAID';
+    $payment_status = 'PAID';
 
- switch ($payment_status) {
-     case 'PAID':
-         echo 'Payment Status: ' . $payment_status . '<br>';
-         break;
-     case 'UNPAID':
-         echo 'Payment Status: ' . $payment_status . '<br>';
-         break;
+    switch ($payment_status) {
+        case 'PAID':
+            echo 'Payment Status: ' . $payment_status . '<br>';
+            break;
+        case 'UNPAID':
+            echo 'Payment Status: ' . $payment_status . '<br>';
+            break;
     case 'PENDING':
         echo 'Payment Status: ' . $payment_status . '<br>';
         break;
-     default:
-         echo 'Payment Status: ' . $payment_status . '<br>';
-         break;
- }
+    case 'REJECTED':
+        echo 'Payment Status: ' . $payment_status . '<br>';
+        break;
+        default:
+            echo 'Payment Status: UNKNOWN' . '<br>';
+            break;
+    }
+
+// Switch Statement with Array and Loop
+
+$payment_status = ['PAID', 'UNPAID', 'REJECTED'];
+    foreach ($payment_status as $status) {
+        switch ($status) {
+            case 'PAID':
+                echo 'Payment Status: ' . $status . '<br>';
+                break;
+            case 'UNPAID':
+                echo 'Payment Status: ' . $status . '<br>';
+                break;
+            case 'PENDING':
+                echo 'Payment Status: ' . $status . '<br>';
+                break;
+            case 'REJECTED':
+                echo 'Payment Status: ' . $status . '<br>';
+                break;
+            default:
+                echo 'Payment Status: UNKNOWN' . '<br>';
+                break;
+        }
+    }
